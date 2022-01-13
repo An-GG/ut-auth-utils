72b9803aa4270d69c6cd32d22df7ed9724a28f3e
An-GG <ankush.girotra@gmail.com>
2022-01-12 21:08:33 -0600

    done

---
+++ README.md
@@ -0,0 +1,38 @@
+# ut-auth-utils
+
+Authenticate into UT Austin applications and retreive session cookies so you can make your own programatic API requests.
+
+> **Note:** Google Chrome must be installed. 
+
+A local copy of Chrome is not installed to keep things lean. **TODO**: Optionally allow installation of local Chrome binary.
+
+## Install
+
+`$ npm i @an-gg/ut-auth-utils`
+
+## Usage
+
+```ts
+import { chromeProgrammaticAuthentication, UT_DIRECT_URL } from 'ut-auth-utils'
+
+let cookies = await chromeProgrammaticAuthentication('UT EID', 'password', UT_DIRECT_URL);
+// You will get 2FA request. Programmatic auth always picks 'Duo Push' as factor
+
+
+// Now you can use the cookies to make your own API calls.
+import fetch from 'node-fetch';
+
+let serialized_cookies = cookies.map(ck=>ck.name+'='+ck.value).join('; ')
+let result = await fetch('https://utdirect.utexas.edu/registration/classlist.WBX', { headers: { cookie:serialized_cookies } });
+```
+
+Alternatively, you can also authenticate graphically from a Chrome window. This method is simpler and more secure, as your node script never has to handle raw user credentials.
+
+```ts
+...
+
+let cookies = await chromeGUIAuthentication(UT_DIRECT_URL);
+
+```
+
+
--- src/auth-utils.ts
+++ src/auth-utils.ts
@@ -1,3 +1,76 @@
+import puppeteer from 'puppeteer-core';
+import { URL } from 'url';
+
+
+export const UT_DIRECT_URL = 'https://utdirect.utexas.edu/';
+export const UT_CANVAS_URL = 'https://utexas.instructure.com/';
+
+
+/**
+ * Login graphically with Google Chrome.
+ * @param relay_url The location from which the login request originates, ex. UT Direct or Canvas
+ */
+export async function chromeGUIAuthentication(relay_url:string) {
+    let chrome = await puppeteer.launch({
+        channel: 'chrome',
+        headless: false,
+        defaultViewport: null
+    });
+    let page = await chrome.newPage();
+    await page.goto(relay_url);
+
+    return _waitForCookies(page, chrome, relay_url);
+}
+
+/**
+ * Login programmatically with Google Chrome.
+ * Note: Duo 2FA will still require user input.
+ */
+export async function chromeProgrammaticAuthentication(username:string, password:string, relay_url:string) {
+
+   let chrome = await puppeteer.launch({
+      channel: 'chrome',
+      headless: false
+   });
+   let page = await chrome.newPage();
+   await page.goto(relay_url);
+   await page.waitForNavigation();
+   await page.waitForSelector('.loginForm');
+   await page.type('#username', username);
+   await page.type('#password', password);
+   await page.click('input[type=submit]');
+   
+   // form error watchdog
+   (async ()=>{
+       let err_element = await page.waitForSelector('.form-error')
+       let err_text = await err_element.evaluate(el => el.textContent, err_element);
+       throw new Error('Login failed. Maybe invalid credentials?\n\nClient Message: \''+err_text+'\'\n\n');
+    })();
+
+   await page.waitForFrame(async (f)=>{
+      return f.url().match(/https:\/\/.+\.duosecurity\.com\/frame\/prompt/) != null;
+   });
+   
+   let duoframe = page.frames()[1];
+   await duoframe.$eval('#auth_methods > fieldset:nth-child(1) > div.row-label.push-label > button', (el)=>{
+     (el as HTMLButtonElement).click(); 
+   });
+
+   return _waitForCookies(page, chrome, relay_url);
+}
+
+async function _waitForCookies(page:puppeteer.Page, chrome:puppeteer.Browser, final_destination: string) {
+    return new Promise<puppeteer.Protocol.Network.Cookie[]>(async (resolve, reject)=>{
+        await page.on('framenavigated', async (event)=>{
+            if ((new URL(page.url())).host == (new URL(final_destination)).host) {
+                let res = await page.cookies();
+                await chrome.close();
+                resolve(res);
+            }
+        });
+    });
+}
+
 
 
 
