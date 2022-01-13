965aabfc258079de67d2dc2dc4bad648fa57a160
An-GG <ankush.girotra@gmail.com>
2022-01-12 21:35:15 -0600

    format

--- src/auth-utils.ts
+++ src/auth-utils.ts
@@ -10,7 +10,7 @@ export const UT_CANVAS_URL = 'https://utexas.instructure.com/';
  * Login graphically with Google Chrome.
  * @param relay_url The location from which the login request originates, ex. UT Direct or Canvas
  */
-export async function chromeGUIAuthentication(relay_url:string) {
+export async function chromeGUIAuthentication(relay_url: string) {
     let chrome = await puppeteer.launch({
         channel: 'chrome',
         headless: false,
@@ -26,41 +26,41 @@ export async function chromeGUIAuthentication(relay_url:string) {
  * Login programmatically with Google Chrome.
  * Note: Duo 2FA will still require user input.
  */
-export async function chromeProgrammaticAuthentication(username:string, password:string, relay_url:string) {
+export async function chromeProgrammaticAuthentication(username: string, password: string, relay_url: string) {
 
-   let chrome = await puppeteer.launch({
-      channel: 'chrome',
-   });
-   let page = await chrome.newPage();
-   await page.goto(relay_url);
-   await page.waitForNavigation();
-   await page.waitForSelector('.loginForm');
-   await page.type('#username', username);
-   await page.type('#password', password);
-   await page.click('input[type=submit]');
-   
-   // form error watchdog
-   (async ()=>{
-       let err_element = await page.waitForSelector('.form-error')
-       let err_text = await err_element.evaluate(el => el.textContent, err_element);
-       throw new Error('Login failed. Maybe invalid credentials?\n\nClient Message: \''+err_text+'\'\n\n');
+    let chrome = await puppeteer.launch({
+        channel: 'chrome',
+    });
+    let page = await chrome.newPage();
+    await page.goto(relay_url);
+    await page.waitForNavigation();
+    await page.waitForSelector('.loginForm');
+    await page.type('#username', username);
+    await page.type('#password', password);
+    await page.click('input[type=submit]');
+
+    // form error watchdog
+    (async () => {
+        let err_element = await page.waitForSelector('.form-error');
+        let err_text = await err_element.evaluate(el => el.textContent, err_element);
+        throw new Error('Login failed. Maybe invalid credentials?\n\nClient Message: \'' + err_text + '\'\n\n');
     })();
 
-   await page.waitForFrame(async (f)=>{
-      return f.url().match(/https:\/\/.+\.duosecurity\.com\/frame\/prompt/) != null;
-   });
-   
-   let duoframe = page.frames()[1];
-   await duoframe.$eval('#auth_methods > fieldset:nth-child(1) > div.row-label.push-label > button', (el)=>{
-     (el as HTMLButtonElement).click(); 
-   });
+    await page.waitForFrame(async (f) => {
+        return f.url().match(/https:\/\/.+\.duosecurity\.com\/frame\/prompt/) != null;
+    });
 
-   return _waitForCookies(page, chrome, relay_url);
+    let duoframe = page.frames()[1];
+    await duoframe.$eval('#auth_methods > fieldset:nth-child(1) > div.row-label.push-label > button', (el) => {
+        (el as HTMLButtonElement).click();
+    });
+
+    return _waitForCookies(page, chrome, relay_url);
 }
 
-async function _waitForCookies(page:puppeteer.Page, chrome:puppeteer.Browser, final_destination: string) {
-    return new Promise<puppeteer.Protocol.Network.Cookie[]>(async (resolve, reject)=>{
-        await page.on('framenavigated', async (event)=>{
+async function _waitForCookies(page: puppeteer.Page, chrome: puppeteer.Browser, final_destination: string) {
+    return new Promise<puppeteer.Protocol.Network.Cookie[]>(async (resolve, reject) => {
+        await page.on('framenavigated', async (event) => {
             if ((new URL(page.url())).host == (new URL(final_destination)).host) {
                 let res = await page.cookies();
                 await chrome.close();
@@ -69,7 +69,3 @@ async function _waitForCookies(page:puppeteer.Page, chrome:puppeteer.Browser, fi
         });
     });
 }
-
-
-
-
